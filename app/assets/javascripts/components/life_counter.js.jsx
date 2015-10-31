cec88bf050c602a055911b984494bfb16959eda4
Yosuke Oka <werdna.b10@gmail.com>
2015-11-01 00:52:44 +0900

    ライフカウンター機能

--- app/assets/javascripts/components/life_counter.js.jsx
+++ app/assets/javascripts/components/life_counter.js.jsx
@@ -1,8 +1,24 @@
 var LifeCounter = React.createClass({
 
+  getInitialState() {
+    return {
+      life: 20
+    };
+  },
+  calcLife: function(n) {
+    return function() {
+      this.setState({life: this.state.life + n})
+    }.bind(this)
+  },
   render: function() {
     return (
-            <div>Hello react!</div>
-           );
+      <div>
+        <div>{this.state.life}</div>
+        <button onClick={this.calcLife(1)}>+1</button>
+        <button onClick={this.calcLife(5)}>+5</button>
+        <button onClick={this.calcLife(-1)}>-1</button>
+        <button onClick={this.calcLife(-5)}>-5</button>
+      </div>
+    );
   }
 });
